2f96b07e5ff828753ce4f082eec4ff07fb4711e5
navneetpal01 <navneetpalgithub@gmail.com>
2024-02-08 00:12:19 +0530

    Second Commit

--- app/src/main/java/com/example/imagepicker/MainActivity.kt
+++ app/src/main/java/com/example/imagepicker/MainActivity.kt
@@ -1,15 +1,48 @@
 package com.example.imagepicker
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.imagepicker.ui.theme.ImagePickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,10 +50,163 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ImagePickerTheme {
-
+                    MultiplePhotoPickerScreen()
             }
         }
     }
+}
+
+@Composable
+fun SinglePhotoPickerScreen() {
+
+    var selectedImageUri by remember{
+        mutableStateOf<Uri?>(null)
+    }
+    var photoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = {
+            selectedImageUri = it
+        }
+    )
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Choose a Beautiful Photo",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = Color.Black
+                )
+            )
+            Spacer(modifier = Modifier.size(20.dp))
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0073E6), contentColor = Color.White),
+                onClick = {
+                    photoPickerLauncher.launch(
+                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                    )
+                }
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                   Icon(imageVector = Icons.Default.AddCircle, contentDescription = null)
+                    Text(
+                        text = "Pick a Photo",
+                        style = TextStyle(
+                            fontSize = 18.sp
+                        )
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.size(20.dp))
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(240.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                model = selectedImageUri,
+                contentDescription = null
+            )
+        }
+    }
+}
+
+
+@Composable
+fun MultiplePhotoPickerScreen() {
+
+    var selectedImageUri by remember{
+        mutableStateOf<List<Uri?>>(emptyList())
+    }
+
+    var multiplePhotoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickMultipleVisualMedia(maxItems = 4),
+        onResult = {
+            selectedImageUri = it
+        }
+    )
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Choose a Beautiful Photo",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = Color.Black
+                )
+            )
+            Spacer(modifier = Modifier.size(20.dp))
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0073E6), contentColor = Color.White),
+                onClick = {
+                    multiplePhotoPickerLauncher.launch(
+                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                    )
+                }
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Icon(imageVector = Icons.Default.AddCircle, contentDescription = null)
+                    Text(
+                        text = "Pick Photos",
+                        style = TextStyle(
+                            fontSize = 18.sp
+                        )
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.size(20.dp))
+            LazyColumn(){
+                items(selectedImageUri){selectedImageUri->
+                    AsyncImage(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(240.dp)
+                            .clip(RoundedCornerShape(8.dp))
+                            .padding(vertical = 4.dp),
+                        model = selectedImageUri,
+                        contentDescription = null,
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun PhotoPickerScreenPreview() {
+    MultiplePhotoPickerScreen()
 }
 
 
